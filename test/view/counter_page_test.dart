803abfacfae1645bb5e2b265e1a338566f4b7bbc
aoskainer <52571512+aoskainer@users.noreply.github.com>
2024-03-20 19:52:29 +0900

    test: WidgetTestを更新しました。

--- test/view/counter_page_test.dart
+++ test/view/counter_page_test.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/view/counter_page.dart';
+import 'package:flutter_app/view_model/counter_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../shared/create_widget_test_provider_scope.dart';
+
 void main() {
   group('CounterPage Tests', () {
-    testWidgets('カウンターのボタンをタップした時に値が増えることを確認する', (WidgetTester tester) async {
-      // ProviderScopeでアプリケーションをラップして状態管理を有効にする
-      await tester.pumpWidget(
-        const ProviderScope(
-          child: MaterialApp(
-            home: CounterPage(),
-          ),
-        ),
+    late ProviderScope providerScope;
+
+    setUp(() {
+      providerScope = createWidgetTestProviderScope(
+        initialLocation: '/counter',
       );
+    });
+
+    testWidgets('カウンターのボタンをタップした時に値が増えることを確認する', (tester) async {
+      await tester.pumpWidget(providerScope);
+      await tester.pumpAndSettle();
+
+      final element = tester.element(find.byType(CounterPage));
+      final container = ProviderScope.containerOf(element);
 
       // 初期のカウンター値が0であることを確認
       expect(find.text('0'), findsOneWidget);
       expect(find.text('1'), findsNothing);
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(0),
+      );
 
       // FABを探してタップする
       await tester.tap(find.byIcon(Icons.add));
@@ -26,6 +38,94 @@ void main() {
       // カウンターの値が1に増加したことを確認
       expect(find.text('0'), findsNothing);
       expect(find.text('1'), findsOneWidget);
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(1),
+      );
+    });
+
+    testWidgets('リストのボタンをタップした時にカウンターがリセットされることを確認する', (tester) async {
+      await tester.pumpWidget(providerScope);
+      await tester.pumpAndSettle();
+
+      final element = tester.element(find.byType(CounterPage));
+      final container = ProviderScope.containerOf(element);
+
+      // 初期のカウンター値が0であることを確認
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(0),
+      );
+
+      // FABを探して3回タップする
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // カウンターの値が3に増加したことを確認
+      expect(find.text('0'), findsNothing);
+      expect(find.text('3'), findsOneWidget);
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(3),
+      );
+
+      // リストボタンを探してタップする
+      await tester.tap(find.byIcon(Icons.list_alt));
+      await tester.pump();
+
+      // カウンターの値が0にリセットされたことを確認
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(0),
+      );
+    });
+
+    testWidgets('ログアウトのボタンをタップした時にカウンターがリセットされることを確認する', (tester) async {
+      await tester.pumpWidget(providerScope);
+      await tester.pumpAndSettle();
+
+      final element = tester.element(find.byType(CounterPage));
+      final container = ProviderScope.containerOf(element);
+
+      // 初期のカウンター値が0であることを確認
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(0),
+      );
+
+      // FABを探して3回タップする
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // カウンターの値が3に増加したことを確認
+      expect(find.text('0'), findsNothing);
+      expect(find.text('3'), findsOneWidget);
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(3),
+      );
+
+      // リストボタンを探してタップする
+      await tester.tap(find.byIcon(Icons.logout));
+      await tester.pump();
+
+      // カウンターの値が0にリセットされたことを確認
+      expect(
+        container.read(counterViewModelProvider).count,
+        equals(0),
+      );
     });
   });
 }
--- test/view/list_page_test.dart
+++ test/view/list_page_test.dart
@@ -1,42 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/view/list_page.dart';
+import 'package:flutter_app/view_model/list_view_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../shared/create_widget_test_provider_scope.dart';
+
 void main() {
   group('ListPage Tests', () {
-    testWidgets('初期状態の画面描画が適切に行われることを確認する', (WidgetTester tester) async {
-      // ProviderScopeでアプリケーションをラップ
-      await tester.pumpWidget(
-        const ProviderScope(
-          child: MaterialApp(
-            home: ListPage(),
-          ),
-        ),
-      );
+    late ProviderScope providerScope;
 
-      // ListViewが存在するか確認
-      expect(find.byType(ListView), findsOneWidget);
-      // 初期状態でリストアイテムが表示されているか確認（仮にリストが空の場合はこの部分を調整）
+    setUp(() {
+      providerScope = createWidgetTestProviderScope(
+        initialLocation: '/list',
+      );
     });
 
-    testWidgets('ボタンをタップした時にリストにアイテムが追加されることを確認する',
-        (WidgetTester tester) async {
-      // ProviderScopeでアプリケーションをラップ
-      await tester.pumpWidget(
-        const ProviderScope(
-          child: MaterialApp(
-            home: ListPage(),
-          ),
-        ),
+    testWidgets('初期状態で3つのStringが存在している', (tester) async {
+      await tester.pumpWidget(providerScope);
+      await tester.pumpAndSettle();
+
+      final element = tester.element(find.byType(ListPage));
+      final container = ProviderScope.containerOf(element);
+
+      // リストに3つのアイテムが存在していることを確認する
+      expect(find.text('Amazon'), findsOneWidget);
+      expect(find.text('Google'), findsOneWidget);
+      expect(find.text('Facebook'), findsOneWidget);
+      expect(
+        container.read(listViewModelProvider).items,
+        equals(['Amazon', 'Google', 'Facebook']),
+      );
+    });
+
+    testWidgets('追加ボタンをタップした時にリストにアイテムが追加される', (tester) async {
+      await tester.pumpWidget(providerScope);
+      await tester.pumpAndSettle();
+
+      final element = tester.element(find.byType(ListPage));
+      final container = ProviderScope.containerOf(element);
+
+      // FABを探してタップする
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // リストにアイテムが追加されていることを確認する
+      expect(find.text('Amazon'), findsOneWidget);
+      expect(find.text('Google'), findsOneWidget);
+      expect(find.text('Facebook'), findsOneWidget);
+      expect(find.text('Apple'), findsOneWidget);
+      expect(
+        container.read(listViewModelProvider).items,
+        equals(['Amazon', 'Google', 'Facebook', 'Apple']),
+      );
+    });
+
+    testWidgets('ログアウトアイコンをタップした時にリストが初期状態に戻る', (tester) async {
+      await tester.pumpWidget(providerScope);
+      await tester.pumpAndSettle();
+
+      final element = tester.element(find.byType(ListPage));
+      final container = ProviderScope.containerOf(element);
+
+      // FABを探して複数回タップする
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // リストが増えていることを確認する
+      expect(find.text('Amazon'), findsOneWidget);
+      expect(find.text('Google'), findsOneWidget);
+      expect(find.text('Facebook'), findsOneWidget);
+      expect(find.text('Apple'), findsExactly(2));
+      expect(
+        container.read(listViewModelProvider).items,
+        equals(['Amazon', 'Google', 'Facebook', 'Apple', 'Apple']),
       );
 
-      // FABをタップ
-      await tester.tap(find.byIcon(Icons.add));
-      await tester.pump(); // リストの更新を待つ
+      // ログアウトボタンを探してタップする
+      await tester.tap(find.byIcon(Icons.logout));
+      await tester.pump();
 
-      // 'Apple'がリストに追加されたことを確認
-      expect(find.text('Apple'), findsOneWidget);
+      // ページ遷移後にリストが初期状態に戻っていることを確認する
+      expect(
+        container.read(listViewModelProvider).items,
+        equals(['Amazon', 'Google', 'Facebook']),
+      );
     });
   });
 }
